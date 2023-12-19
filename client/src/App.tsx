4a282f23a3b33436e664f14c3d8e23ef768434a4
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2023-12-18 16:15:09 -0800

    added react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage';
+import SignupPage from './components/SignupPage';
+
 function App() {
-  return 'Hello World';
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ client/src/components/SignupPage.tsx
@@ -0,0 +1,5 @@
+const SignupPage = () => {
+  return <h1>Signup</h1>;
+};
+
+export default SignupPage;
