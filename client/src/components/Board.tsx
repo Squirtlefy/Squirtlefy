496092ff06f3ad29ee6e29597e4d8d3d603a36ba
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2023-12-19 14:41:24 -0800

    pushing current version

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const team = [
   'Austin Cavanagh',
   'Jerry Trinh',
@@ -12,37 +14,48 @@ const curBoard = {
     {
       name: 'Card 1',
       tasks: [
-        { name: 'Task 1', people: 'AC' },
-        { name: 'Task 2', people: 'JT' },
-        { name: 'Task 3', people: 'JD' },
-        { name: 'Task 4', people: 'KW' },
-        { name: 'Task 5', people: 'SK' },
+        { name: 'Task 1', people: ['AC'] },
+        { name: 'Task 2', people: ['JT'] },
+        { name: 'Task 3', people: ['JD'] },
+        { name: 'Task 4', people: ['KW'] },
+        { name: 'Task 5', people: ['SK'] },
       ],
     },
     {
       name: 'Card 2',
       tasks: [
-        { name: 'Task 1', people: 'AC' },
-        { name: 'Task 2', people: 'JT' },
-        { name: 'Task 3', people: 'JD' },
-        { name: 'Task 4', people: 'KW' },
-        { name: 'Task 5', people: 'SK' },
+        { name: 'Task 1', people: ['AC'] },
+        { name: 'Task 2', people: ['JT'] },
+        { name: 'Task 3', people: ['JD'] },
+        { name: 'Task 4', people: ['KW'] },
+        { name: 'Task 5', people: ['SK'] },
       ],
     },
     {
       name: 'Card 3',
       tasks: [
-        { name: 'Task 1', people: 'AC' },
-        { name: 'Task 2', people: 'JT' },
-        { name: 'Task 3', people: 'JD' },
-        { name: 'Task 4', people: 'KW' },
-        { name: 'Task 5', people: 'SK' },
+        { name: 'Task 1', people: ['AC'] },
+        { name: 'Task 2', people: ['JT'] },
+        { name: 'Task 3', people: ['JD'] },
+        { name: 'Task 4', people: ['KW'] },
+        { name: 'Task 5', people: ['SK'] },
       ],
     },
   ],
 };
 
 const Board = () => {
+  const [addList, setAddList] = useState(false);
+  const [addCard, setAddCard] = useState(false);
+
+  //   const addListHandleclick = () => {
+  //     setAddList(true);
+  //   };
+
+  const addCardHandleclick = () => {
+    setAddCard(true);
+  };
+
   return (
     <>
       <h1>{curBoard.name}</h1>
@@ -58,11 +71,31 @@ const Board = () => {
                   </div>
                 );
               })}
-              <div className="card add-card">+ Add Card</div>
+              <button
+                className="card add-card-button"
+                onClick={() => console.log('added card')}
+              >
+                + Add Card
+              </button>
             </div>
           );
         })}
-        <div className="add-list">+ Add List</div>
+
+        {!addList && (
+          <button className="add-list-button" onClick={() => setAddList(true)}>
+            + Add List
+          </button>
+        )}
+
+        {addList && (
+          <form className="list" onSubmit={addCardHandleclick}>
+            <input className="add-list-input" type="text" />
+            <div className="add-list-bottom">
+              <button className="submit-button">Add List</button>
+              <button className="cancel-button">X</button>
+            </div>
+          </form>
+        )}
       </div>
     </>
   );
