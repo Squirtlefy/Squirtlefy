d7fbc25675a08d02f10fd4597e05ce6cabbfe406
keidy9 <keidyw999@gmail.com>
2023-12-19 15:10:13 -0800

    start oauth >> >> >> Co-authored-by: Austin <austin.cavanagh.cs@gmail.com> >> Co-authored-by: Jerry <jtjerrytrinh@gmail.com> >> Co-authored-by: John<johnwdunn20@gmail.com> >> Co-authored-by: Keidy <keidyw999@gmail.com> >> Co-authored-by: Sean <kirkpatrick.sk@gmail.com>

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,67 @@
+const { google } = require('googleapis');
 const express = require('express');
+const cors = require('cors');
 const PORT = 3000;
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+// client_ID = '207130399105-liobtrmdselqr1um3qe1188d2c2djnrn.apps.googleusercontent.com'
+// client_SECRET = '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'
+
+const oauth2Client = new google.auth.OAuth2(
+  '207130399105-liobtrmdselqr1um3qe1188d2c2djnrn.apps.googleusercontent.com',
+  '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',
+  'http://localhost:3000/login'
+);
+
+// set auth as a global default
+google.options({
+  auth: oauth2Client
+});
+
+oauth2Client.on('tokens', (tokens) => {
+  if (tokens.refresh_token) {
+    // store the refresh_token in my database!
+    console.log(tokens.refresh_token);
+  }
+  console.log(tokens.access_token);
+});
+
+const scopes = [
+  'https://www.googleapis.com/auth/userinfo.profile',
+  'https://www.googleapis.com/auth/userinfo.email',
+];
+
+const url = oauth2Client.generateAuthUrl({
+  access_type: 'offline',
+  scope: scopes,
+});
+
 // TEST - can see output if you open localhost 3000 in browser
 app.get('/', (req: any, res: any) => {
-  res.send('Hello!')
-})
+  res.redirect(url);
+});
 
+app.get('/login', async (req: any, res: any) => {
+  console.log(req.query);
+  const { code } = req.query;
+  const { tokens } = await oauth2Client.getToken(code);
+  oauth2Client.setCredentials(tokens);
+
+  const response = await fetch(
+    `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${tokens}`
+  );
+
+  const data = await response.json();
+  console.log('data: ', data);
+
+  res.redirect('http://localhost:5173/');
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
-})
+});
 
 module.exports = app;
